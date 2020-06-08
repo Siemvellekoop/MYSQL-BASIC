6e4c26937aa42068b04387fc39899ed6f9a0aa46
Siemvellekoop <Siemvellekoop@hotmail.nl>
2020-06-08 14:04:40 +0200

    Klaar

--- 2-Select/taak02/antwoorden.sql
+++ 2-Select/taak02/antwoorden.sql
@@ -1,19 +1,22 @@
 -- Opdracht 1 
-
+Netherlands
+Western Europe
+7
+7378
 -- Opdracht 2 
-
+SELECT * FROM `jaar2016` WHERE country="Netherlands"
 -- Opdracht 3
-
+SELECT * FROM `jaar2015` WHERE country="Netherlands"
 -- Opdracht 4
-
+SELECT * FROM `jaar2016` WHERE country="Netherlands"
 -- Opdracht 5
-
+SELECT * FROM `jaar2015` WHERE region="North America"
 -- Opdracht 6 
-
+SELECT country FROM `jaar2016` WHERE rank=25 
 -- Opdracht 7 
-
+SELECT country FROM `jaar2016` WHERE score=>7087
 -- Opdracht 8
-
+SELECT country FROM `jaar2016` WHERE rank=<5
 -- Opdracht 9
 
 -- Opdracht 10

87c2ab2e0554288ddfe81145ff88a014b10a208c
Siemvellekoop <Siemvellekoop@hotmail.nl>
2020-06-24 09:59:33 +0200

    AF

--- opdracht/antwoorden.sql
+++ opdracht/antwoorden.sql
@@ -7,13 +7,14 @@ SELECT * FROM videogamesales WHERE genre = 'Sports' AND NA_Sales
 -- Opdracht 4
 SELECT name, platform FROM videogamesales WHERE publisher= "Nintendo" AND year >= 1995 AND year <= 2005;
 -- Opdracht 5
+SELECT MAX(Global_Sales), name FROM videogamesales 
 Wii Sports
 -- Opdracht 6 
 SELECT AVG(EU_Sales) FROM videogamesales WHERE genre = 'Puzzle' 
 -- Opdracht 7 
-SELECT * FROM videogamesales WHERE JP_Sales = '-MAX'
+SELECT MIN(JP_Sales), name FROM videogamesales
 -- Opdracht 8
-SELECT Global_Sales FROM `videogamesales` WHERE publisher="Nintendo";
+SELECT SUM(Global_Sales) FROM `videogamesales` WHERE publisher="Nintendo"
 -- Opdracht 9
 SELECT name, year FROM `videogamesales` WHERE `publisher`="Nintendo" + "Activision" AND `genre`="Racing";
 -- Opdracht 10
@@ -25,6 +26,6 @@ DELETE FROM videogamesales WHERE year = "2012" AND publisher = "Ubisoft";
 -- Opdracht 13
 DELETE FROM videogamesales WHERE genre = "Adventure" AND publisher = "Nintendo";
 -- Opdracht 14
-DELETE FROM videogamesales WHERE publisher = "Nintendo" AND `Global_Sales`= <1000;
+DELETE FROM videogamesales WHERE publisher = "Nintendo" AND `Global_Sales`<=1000;
 -- Opdracht 15
-DELETE FROM videogamesales WHERE year = "1997" AND `NA_Sales`= >200000;
+DELETE FROM videogamesales WHERE year = "1997" AND `NA_Sales`>=200000;
